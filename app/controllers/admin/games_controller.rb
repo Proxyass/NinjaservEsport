0393e5dfaba8e06a96964011792bee9cfd2c28d1
Clément Hampaï <clement.hampai@cypressxt.net>
2018-01-04 17:26:25 +0100

    Game creation process

---
+++ app/controllers/admin/games_controller.rb
@@ -0,0 +1,44 @@
+class Admin::GamesController < Admin::BaseAdminController
+  before_action :game_must_exist,   except: [:new, :create]
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      flash[:success] = "Game "+@game.name.capitalize+" successfully created."
+      redirect_to new_admin_game_path
+    else
+      flash.now[:danger] =  "Failed to create "+@game.name + ", "+@game.errors.full_messages.to_sentence+"."
+      render 'new'
+    end
+  end
+
+  def delete
+  end
+
+  def destroy
+    if @game.destroy
+      flash[:success] = "Game "+@game.name.capitalize+" successfully deleted."
+      redirect_to games_path
+    else
+      flash.now[:danger] =  "Failed to delete "+@game.name + ", "+@game.errors.full_messages.to_sentence+"."
+      render 'delete'
+    end
+  end
+
+  def game_must_exist
+    @game = Game.find_by(id: params[:game_id]) || Game.find_by(id: params[:id])
+    if !@game
+      render_404
+    end
+  end
+
+
+  def game_params
+    params.require(:game).permit(:name, :description, :team_based, :image_url)
+  end
+
+end
